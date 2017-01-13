7247effe21522c76480a0388664116c0ca409441
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2017-01-13 23:22:11 +0100

    wizytowka

--- R/wizytowka.R
+++ R/wizytowka.R
@@ -9,7 +9,10 @@
 
 wizytowka <- function(strategia){
   #dane do wizytówki
-  przebieg_gry <- SuperFarmer.SuperDziewczyn::gra(strategia)
+  library(plyr)
+  library(emojifont)
+  library(ggplot2)
+  przebieg_gry <- SuperFarmer.SuperDziewczyn::gra(strategia_owce)
   macierz_przebiegu_gry <- przebieg_gry[[1]]
   przebieg_100_gier_superdziewczyn <- SuperFarmer.SuperDziewczyn::badaj_gre(strategia_owce,powtorzenia = 100)
   przebieg_100_gier_moc <- SuperFarmerMoc::badaj_gre(SuperFarmerMoc::strategia_postMDiPR,liczba_prob=100)
@@ -46,14 +49,19 @@ wizytowka <- function(strategia){
   wykres_gestosc <- ggplot(przebieg, aes(Liczba_ruchow,..count..,colour=Pakiet))+geom_density(position="stack")+scale_color_manual(values=c("blue","green","red"))+geom_vline(data=srednia, aes(xintercept=grp.mean, color=Pakiet),linetype="dashed")
   
   
-  #tu mam dolozyc emoji
-  przebieg_gry_owce <- ggplot2::ggplot(przebieg_gry,(ggplot2::aes(numer_kolejki,owca)))+ggplot2::geom_point()
+  #tu mam dolozyc emoji, trzeba pobrac emoji z githuba, ale nadal nie dziala
+
+  load.emojifont('OpenSansEmoji.ttf')
+  owce <- przebieg_gry[,c(2,8)]
+  owce <- data.frame(owce,zwierzeta=sample(emoji("sheep"),nrow(owce),replace=TRUE))
+  przebieg_gry_owce <- ggplot(przebieg_gry,(aes(numer_kolejki,owca, label=zwierzeta)))+geom_point(size=0)+ geom_text(family="OpenSansEmoji", size=6)
+ 
   przebieg_100_gier <- SuperFarmer.SuperDziewczyn::wizualizacja_gry(przebieg_100_gier_superdziewczyn, "histogram")
   
   
   #to co chcemy dolozyc jako tekst
   tekst <- grid::textGrob("Ola i Asia")
-  oba <-gridExtra::grid.arrange(tekst,przebieg_gry_owce,przebieg_100_gier,wykres_gestosc,ncol=2) #na rownych skalach
+  oba <-gridExtra::grid.arrange(tekst,przebieg_gry_owce,wykres_gestosc,ncol=2) #na rownych skalach
   
   
   ggplot2::ggsave("strategia_owce.pdf",oba)
